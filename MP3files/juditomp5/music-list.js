e6fce53b95345940385991777b58f2664edd5c93
judsssss <106899865+judsssss@users.noreply.github.com>
2024-05-20 22:11:05 +0800

    Add files via upload

---
+++ MP3files/juditomp5/music-list.js
@@ -0,0 +1,56 @@
+let allMusic = [
+    {
+        name: "Maharani",
+        artist: "ALAMAT",
+        img: "alamat",
+        scr: "Music-1"
+    },
+    {
+        name: "Aphrodite",
+        artist: "The Ridleys",
+        img: "the ridleys",
+        scr: "Music-2"
+    },
+    {
+        name: "Salamin",
+        artist: "BINI",
+        img: "bini",
+        scr: "Music-3"
+    },    {
+        name: "Easy",
+        artist: "Le Sserafim",
+        img: "Le Sserafim",
+        scr: "Music-4"
+    },    {
+        name: "Hurt",
+        artist: "NewJeans",
+        img: "newjeans",
+        scr: "Music-5"
+    },    {
+        name: "Take A Chance With Me",
+        artist: "Nikki",
+        img: "nikki",
+        scr: "Music-6"
+    },    {
+        name: "Fck MyLife",
+        artist: "Seventeen",
+        img: "Seventeen",
+        scr: "Music-7"
+    },    {
+        name: "Sikulo",
+        artist: "Maki, Angela Ken, Nhiko",
+        img: "maki",
+        scr: "Music-8"
+    },    {
+        name: "Karera",
+        artist: "BINI",
+        img: "BiniKarera",
+        scr: "Music-9"
+    },
+    {
+        name: "Pantropiko",
+        artist: "BINI",
+        img: "BiniPantropiko",
+        scr: "Music-10"
+    }
+ ]
